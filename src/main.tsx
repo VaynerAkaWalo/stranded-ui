07a8a28a62395c395ef80fc3b2213025b1f76f46
VaynerAkaWalo <vaynerakawalo@gmail.com>
2025-10-28 23:54:45 +0100

    Style order

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App.tsx";
 import GlobalWrapper from "@components/wrapper/global-wrapper.tsx";
 import ActionWrapper from "@components/action/action-wrapper.tsx";
+import { GlobalStyles, StyledEngineProvider } from "@mui/material";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GlobalWrapper>
-      <ActionWrapper>
-        <App />
-      </ActionWrapper>
-    </GlobalWrapper>
+    <StyledEngineProvider enableCssLayer>
+      <GlobalWrapper>
+        <ActionWrapper>
+          <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+          <App />
+        </ActionWrapper>
+      </GlobalWrapper>
+    </StyledEngineProvider>
   </StrictMode>,
 );
